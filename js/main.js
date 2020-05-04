5a70bd7a76d08d7f02d305b6d1abcbc6098651d5
Marcepan576 <marcepan.lawa576@gmail.com>
2020-05-04 22:28:17 +0200

    gg
    c

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function proponowane1() {
+
+document.getElementById("link2").setAttribute("id", "link2Show");
+document.getElementById("link1").setAttribute("id", "link1hide");
+document.getElementById("przewin").setAttribute("id", "link1hide");
+
+};
