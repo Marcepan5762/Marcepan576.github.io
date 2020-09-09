b66e99815ab551165c88fb99d0b8dbbd2095dd2b
Marcepan576 <marcepan.lawa576@gmail.com>
2020-09-09 18:31:05 +0200

    animations

--- js/main.js
+++ js/main.js
@@ -1,25 +1,2 @@
 
 
-var x = 0;
-function apl2() {
-
-    $('#link1').css('display', 'none');
-    $('#link2').show();
-
-};
-
-function apl1() {
-
-    $('#link2').css('display', 'none');
-    $('#link1').show();
-
-};
-
-setInterval(function() {
-
-    var i = Math.floor(Math.random() * 2) + 1;
-
-    window['apl' + i]();
-    
-
-}, 7000);
